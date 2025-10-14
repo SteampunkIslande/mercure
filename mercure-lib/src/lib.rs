06e3987aebf10fdf73d1bbac2e4790a3433f36db
SteampunkIslande <charles.monod-broca@neuf.fr>
2025-10-14 17:40:48 +0200

    WIP

--- mercure-lib/src/lib.rs
+++ mercure-lib/src/lib.rs
@@ -2,5 +2,6 @@ pub mod auth;
 pub mod config;
 pub mod db;
 pub mod models;
+pub mod pipeline_exec;
 pub mod routes;
 pub mod utils;
---
+++ mercure-lib/src/pipeline_exec/mod.rs
@@ -0,0 +1,7 @@
+pub mod cancel_job;
+pub mod follow_job;
+pub mod get_job_logs;
+
+pub use cancel_job::*;
+pub use follow_job::*;
+pub use get_job_logs::*;
