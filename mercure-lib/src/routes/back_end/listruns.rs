4370c34df13f51429815b91c1633565811c49b8d
SteampunkIslande <charles.monod-broca@neuf.fr>
2025-12-17 10:09:37 +0100

    Fixed cosmetic

--- mercure-lib/src/routes/back_end/listruns.rs
+++ mercure-lib/src/routes/back_end/listruns.rs
@@ -37,12 +37,12 @@ fn create_run_row(
         // Colonne 2 - Utilisateur
         json!({
             "content": user_name,
-            "td_class": "badge-column"
+            "td_class": "content-column"
         }),
         // Colonne 3 - Date du run
         json!({
             "content": format_french_date(run_date),
-            "td_class": "badge-column"
+            "td_class": "content-column"
         }),
         // Colonne 4 - Statut
         json!({
