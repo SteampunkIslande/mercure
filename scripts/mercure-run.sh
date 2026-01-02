12adc438f1be89ae898c91dd3559f626c69b8d1e
SteampunkIslande <charles.monod-broca@neuf.fr>
2026-01-02 18:39:56 +0100

    Updated mercure-run script

--- scripts/mercure-run.sh
+++ scripts/mercure-run.sh
@@ -6,8 +6,10 @@
 
 [[ $USER != "hermes" ]] && echo "Ce script doit être exécuté en tant qu'utilisateur 'hermes'" && exit 1
 
-MERCURE_WEBAPP_SIF="/OPT/mercure/mercure-webapp.sif"
-MERCURE_ROUTINE_SIF="/OPT/mercure/mercure-routine.sif"
+MERCURE_DIR="/OPT/mercure"
+
+MERCURE_WEBAPP_SIF="$MERCURE_DIR/mercure-webapp.sif"
+MERCURE_ROUTINE_SIF="$MERCURE_DIR/mercure-routine.sif"
 
 JOBS_DIR="/OPT/JOBS"
 PIPELINES_DIR="/OPT/pipelines"
@@ -15,7 +17,7 @@ PIPELINES_DIR="/OPT/pipelines"
 RUNNING_WEBAPP=$(singularity instance list | sed 1d | cut -d ' ' -f1 | grep mercure-webappd)
 
 if [[ -z "$RUNNING_WEBAPP" ]];then
-    singularity instance start $MERCURE_WEBAPP_SIF -B /OPT/mercure -B $JOBS_DIR -B $PIPELINES_DIR mercure-webappd
+    singularity instance start $MERCURE_WEBAPP_SIF -B $MERCURE_DIR -B $JOBS_DIR -B $PIPELINES_DIR mercure-webappd
     success=$?
     if [[ $success -ne 0 ]]; then
         echo "Échec du démarrage de l'instance mercure-webappd. La routine n'a pas été démarrée."
@@ -30,7 +32,7 @@ fi
 RUNNING_ROUTINE=$(singularity instance list | sed 1d | cut -d ' ' -f1 | grep mercure-routined)
 
 if [[ -z "$RUNNING_ROUTINE" ]];then
-    singularity instance start $MERCURE_ROUTINE_SIF -B /OPT/mercure -B $JOBS_DIR -B $PIPELINES_DIR mercure-routined
+    singularity instance start $MERCURE_ROUTINE_SIF -B $MERCURE_DIR -B $JOBS_DIR -B $PIPELINES_DIR mercure-routined
     success=$?
     if [[ $success -ne 0 ]]; then
         echo "Échec du démarrage de l'instance mercure-routined"
