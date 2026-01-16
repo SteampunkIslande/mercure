7c27aeea29582481e06d543171ac0f9f186bac62
SteampunkIslande <charles.monod-broca@neuf.fr>
2026-01-16 23:18:42 +0100

    Fixed shebang for run completed script

--- scripts/run-completed.sh
+++ scripts/run-completed.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/ash
+
+# Pourquoi ash ? Parce que c'est le shell par défaut sur les images Docker légères basées sur Alpine Linux (et que ces dernières n'ont pas bash installé).
+
 # Ce script reçoit deux arguments: le chemin absolu du répertoire de run brut, et le nom du séquenceur (qui fait partie normalement du chemin).
 # Il doit retourner 1 si le dossier de run passé en argument n'est pas celui d'un run terminé. Autrement, le script retourne 0.
 SEQ_DIR=$1
